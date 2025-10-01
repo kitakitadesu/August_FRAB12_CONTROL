b89422cc320c5df6df53ebcc9fe54f353b426b67
Bocchi <bookshorse@outlook.jp>
2025-10-02 01:23:56 +0700

    implement main publishers subscribers

--- LOW_LEVEL/src/main.cpp
+++ LOW_LEVEL/src/main.cpp
@@ -6,20 +6,35 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
-#include <std_msgs/msg/int32.h>
+#include "../lib/DCMotor/dc_motor.hpp"
+#include "../lib/IncrementalMotorEncoder/incremental_motor_encoder.hpp"
+
+#include "nodes/publishers/encoder_publisher.hpp"
+#include "nodes/subscribers/cmd_vel_subscriber.hpp"
 
 #if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
-#error This example is only avaliable for Arduino framework with serial transport.
+#error Only avaliable for Arduino framework with serial transport.
 #endif
-
-rcl_publisher_t publisher;
-std_msgs__msg__Int32 msg;
 
 rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
-rcl_timer_t timer;
+
+// Motor and Encoder instances
+DCMotor motorM1(0, 1, 2);    // GP0, GP1, GP2
+DCMotor motorM2(3, 4, 5);    // GP3, GP4, GP5
+DCMotor motorM3(6, 7, 8);    // GP6, GP7, GP8
+DCMotor motorM4(9, 10, 11);  // GP9, GP10, GP11
+
+IncrementalMotorEncoder encoderM1(motorM1, 12, 13);   // GP12, GP13
+IncrementalMotorEncoder encoderM2(motorM2, 14, 15);   // GP14, GP15
+IncrementalMotorEncoder encoderM3(motorM3, 16, 17);   // GP16, GP17
+IncrementalMotorEncoder encoderM4(motorM4, 18, 19);   // GP18, GP19
+
+// ROS2 Node instances
+EncoderPublisher encoder_publisher(encoderM1, encoderM2);
+CmdVelSubscriber cmd_vel_subscriber(encoderM1, encoderM2, encoderM3, encoderM4);
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
@@ -28,14 +43,6 @@ rcl_timer_t timer;
 void error_loop() {
   while(1) {
     delay(100);
-  }
-}
-
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
-  RCLC_UNUSED(last_call_time);
-  if (timer != NULL) {
-    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-    msg.data++;
   }
 }
 
@@ -49,34 +56,31 @@ void setup() {
   allocator = rcl_get_default_allocator();
 
   //create init_options
-  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+  rclc_support_init(&support, 0, NULL, &allocator);
 
   // create node
-  RCCHECK(rclc_node_init_default(&node, "micro_ros_platformio_node", "", &support));
+  rclc_node_init_default(&node, "micro_ros_platformio_node", "", &support);
 
-  // create publisher
-  RCCHECK(rclc_publisher_init_default(
-    &publisher,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "micro_ros_platformio_node_publisher"));
+  // create executor (now handles 2 handles: timer and subscriber)
+  rclc_executor_init(&executor, &support.context, 2, &allocator);
 
-  // create timer,
-  const unsigned int timer_timeout = 1000;
-  RCCHECK(rclc_timer_init_default(
-    &timer,
-    &support,
-    RCL_MS_TO_NS(timer_timeout),
-    timer_callback));
+  // Setup ROS2 nodes
+  encoder_publisher.setup(&node, &support, &executor);
+  cmd_vel_subscriber.setup(&node, &support, &executor);
 
-  // create executor
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  // Initialize motors and encoders
+  motorM1.setup();
+  motorM2.setup();
+  motorM3.setup();
+  motorM4.setup();
 
-  msg.data = 0;
+  encoderM1.setup();
+  encoderM2.setup();
+  encoderM3.setup();
+  encoderM4.setup();
 }
 
 void loop() {
   delay(100);
-  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
 }
